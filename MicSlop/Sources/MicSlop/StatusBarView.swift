4607bd3872edd044ab68428d39ec2ade94ec732c
fozcodes <ian.fosbery@paramount.com>
2026-04-23 21:27:47 -0400

    Add StatusBarView with ON AIR / OFF AIR display

---
+++ MicSlop/Sources/MicSlop/StatusBarView.swift
@@ -0,0 +1,23 @@
+import SwiftUI
+
+struct StatusBarView: View {
+    let isOnAir: Bool
+    
+    var body: some View {
+        Text(isOnAir ? "ON AIR" : "OFF AIR")
+            .font(.system(size: 10, weight: .bold))
+            .foregroundColor(.white)
+            .padding(.horizontal, 6)
+            .padding(.vertical, 2)
+            .background(isOnAir ? Color(red: 1.0, green: 0.231, blue: 0.188) : Color(white: 0.557))
+            .cornerRadius(3)
+    }
+}
+
+#Preview {
+    VStack(spacing: 10) {
+        StatusBarView(isOnAir: true)
+        StatusBarView(isOnAir: false)
+    }
+    .padding()
+}
