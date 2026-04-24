b8724b31b43f5c2d00cb2eb01f00af1e627431d0
fozcodes <ian.fosbery@paramount.com>
2026-04-23 21:30:51 -0400

    Add MicSlopApp with menu bar, hotkey, and launch-at-login

---
+++ MicSlop/Sources/MicSlop/MicSlopApp.swift
@@ -0,0 +1,82 @@
+import SwiftUI
+import HotKey
+import ServiceManagement
+import Combine
+
+@main
+struct MicSlopApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
+    var body: some Scene {
+        Settings {
+            EmptyView()
+        }
+    }
+}
+
+@MainActor
+final class AppDelegate: NSObject, NSApplicationDelegate {
+    private var statusItem: NSStatusItem!
+    private var audioController: AudioController!
+    private var hotKey: HotKey?
+    private var cancellables = Set<AnyCancellable>()
+    
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        audioController = AudioController()
+        
+        setupStatusItem()
+        setupHotKey()
+        setupLaunchAtLogin()
+        
+        observeVolumeChanges()
+    }
+    
+    private func setupStatusItem() {
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        
+        updateStatusItemView()
+        
+        let menu = NSMenu()
+        menu.addItem(NSMenuItem(title: "Quit", action: #selector(quit), keyEquivalent: "q"))
+        statusItem.menu = menu
+    }
+    
+    private func updateStatusItemView() {
+        let isOnAir = audioController.volume > 0
+        let hostingView = NSHostingView(rootView: StatusBarView(isOnAir: isOnAir))
+        hostingView.frame = NSRect(x: 0, y: 0, width: 60, height: 22)
+        statusItem.button?.subviews.forEach { $0.removeFromSuperview() }
+        statusItem.button?.addSubview(hostingView)
+        statusItem.button?.frame = hostingView.frame
+    }
+    
+    private func setupHotKey() {
+        hotKey = HotKey(key: .l, modifiers: [.command])
+        hotKey?.keyDownHandler = { [weak self] in
+            Task { @MainActor in
+                self?.audioController.toggle()
+            }
+        }
+    }
+    
+    private func setupLaunchAtLogin() {
+        do {
+            try SMAppService.mainApp.register()
+        } catch {
+            print("Failed to register launch at login: \(error)")
+        }
+    }
+    
+    private func observeVolumeChanges() {
+        audioController.$volume
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] _ in
+                self?.updateStatusItemView()
+            }
+            .store(in: &cancellables)
+    }
+    
+    @objc private func quit() {
+        NSApplication.shared.terminate(nil)
+    }
+}
